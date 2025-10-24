c0bbc4b3cb9ea5577da2bb28e2f46dc05d8f1bbf
Evan Carlson <ecarlson2020@gmail.com>
2025-10-24 11:42:19 -0600

    .eslintrc.js update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,25 @@ module.exports = {
     "plugin:prettier/recommended",
   ],
   rules: {
+    "max-lines": [
+      "error",
+      { max: 300, skipBlankLines: false, skipComments: false },
+    ],
     "no-console": [1, { allow: ["warn", "error"] }],
-    "no-unused-vars": 1,
+    "no-nested-ternary": 0,
     "prettier/prettier": 1,
+    "import/prefer-default-export": 0,
+    "react/require-default-props": 0,
+    "jsx-a11y/no-autofocus": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
     "react/jsx-filename-extension": 0,
     "react/jsx-props-no-spreading": 0,
     "react/no-array-index-key": 0,
-    "no-param-reassign": 0, // for redux
+    "no-param-reassign": 0,
     "no-shadow": 0,
-    "arrow-body-style": ["error", "as-needed"],
+    "arrow-body-style": ["warn", "as-needed"],
     // for next.js
     "react/react-in-jsx-scope": 0,
     "@next/next/no-img-element": 0,
@@ -28,7 +38,7 @@ module.exports = {
       },
     ],
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"]
+    "@typescript-eslint/no-unused-vars": ["error"],
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
