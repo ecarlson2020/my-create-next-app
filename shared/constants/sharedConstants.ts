c94b0e803bad260c99fd71a897c0245276adc304
Evan Carlson <ecarlson2020@gmail.com>
2025-12-31 14:51:28 -0700

    Refactor environment variable handling in shared constants for improved clarity and maintainability

--- shared/constants/sharedConstants.ts
+++ shared/constants/sharedConstants.ts
@@ -5,18 +5,19 @@ const productionDomain = "adamselectric";
 const prodPort = 5009;
 const devPort = 5003;
 
-export const IS_STAGING = process.env.MY_ENV === "staging";
+const isStaging = process.env.MY_ENV === "staging";
+
 export const IS_PROD = process.env.MY_ENV === "production";
-export const IS_DEV = !IS_PROD && !IS_STAGING;
+export const IS_DEV = !IS_PROD && !isStaging;
 export const PORT = IS_PROD ? prodPort : devPort;
 export const DOMAIN = IS_PROD ? productionDomain : stagingDomain;
 export const WEB_URL = IS_PROD
-  ? `https://www.${productionDomain}.com`
-  : IS_STAGING
+  ? `https://${productionDomain}.com`
+  : isStaging
     ? `https://${stagingDomain}.com`
     : "http://localhost:5001";
 export const API_URL = IS_PROD
   ? `https://${productionDomain}.com:${PORT}`
-  : IS_STAGING
+  : isStaging
     ? `https://${stagingDomain}.com:${PORT}`
     : `http://localhost:${PORT}`;
